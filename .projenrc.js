da248d11891873de53763cc77ccb6a66f7950b08
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2026-02-04 22:21:54 +0100

    upgrade projen version

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,73 @@
-const { GradleLibraryProject } = require('@gplassard/projen-extensions');
+const { GradleLibraryProject, WorkflowActionsX } = require('@gplassard/projen-extensions');
+const { github } = require('projen');
 
 const project = new GradleLibraryProject({
    name: 'smithy-extensions',
 });
+
+const releaseWorkflow = project.github.addWorkflow('release-open-api');
+releaseWorkflow.on({
+  push: {
+    tags: ['open-api-*'],
+  },
+});
+
+// Build and publish to GitHub Packages
+releaseWorkflow.addJob('build-github', {
+  runsOn: ['ubuntu-latest'],
+  env: {
+    CODE_ARTIFACT_URL: '${{ secrets.CODE_ARTIFACT_URL }}',
+  },
+  permissions: {
+    contents: github.workflows.JobPermission.READ,
+    packages: github.workflows.JobPermission.WRITE,
+    idToken: github.workflows.JobPermission.WRITE,
+  },
+  steps: [
+    WorkflowActionsX.checkout(),
+    WorkflowActionsX.setupJdk({}),
+    {
+      name: 'Build',
+      run: './gradlew build',
+    },
+    {
+      name: 'Publish package',
+      run: './gradlew :open-api:publishAllPublicationsToGithubPackagesRepository',
+      env: {
+        GITHUB_TOKEN: '${{ secrets.GITHUB_TOKEN }}',
+      },
+    },
+  ],
+});
+
+// Build and publish to AWS CodeArtifact
+releaseWorkflow.addJob('build-code-artifact', {
+  runsOn: ['ubuntu-latest'],
+  env: {
+    CODE_ARTIFACT_URL: '${{ secrets.CODE_ARTIFACT_URL }}',
+  },
+  permissions: {
+    contents: github.workflows.JobPermission.READ,
+    packages: github.workflows.JobPermission.WRITE,
+    idToken: github.workflows.JobPermission.WRITE,
+  },
+  steps: [
+    WorkflowActionsX.checkout(),
+    WorkflowActionsX.setupJdk({}),
+    {
+      name: 'Build',
+      run: './gradlew build',
+    },
+    WorkflowActionsX.configureAwsCredentials('${{ secrets.CODE_ARTIFACT_WRITE_ROLE }}'),
+    WorkflowActionsX.generateCodeArtifactToken(),
+    {
+      name: 'Publish package',
+      run: './gradlew :open-api:publishAllPublicationsToCodeArtifactRepository',
+      env: {
+        CODEARTIFACT_AUTH_TOKEN: '${{ steps.code-artifact-token.outputs.token }}',
+      },
+    },
+  ],
+});
+
 project.synth();
